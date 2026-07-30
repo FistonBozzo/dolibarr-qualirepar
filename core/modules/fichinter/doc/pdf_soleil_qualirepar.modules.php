4f34b3dc0ad26b5b1f3147fac911382e36a2bf32
FistonBozzo <gelay.julien@gmail.com>
2026-07-30 12:33:54 +0200

    Update pdf_soleil_qualirepar.modules.php

--- core/modules/fichinter/doc/pdf_soleil_qualirepar.modules.php
+++ core/modules/fichinter/doc/pdf_soleil_qualirepar.modules.php
@@ -329,7 +329,7 @@ class pdf_soleil_qualirepar extends ModelePDFFicheinter
 				$pdf->SetFont('', '', 8);
 				$pdf->Cell($colW - $labelW - 5, 4, price($object->array_options['options_budget_max']));
 
-				$tab_top += 32;
+				$tab_top += 20;
 
 
 				
