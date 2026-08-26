74aeb53c4654bc9a08cc0254fb29c6d83ed1e0b4
FistonBozzo <gelay.julien@gmail.com>
2026-08-26 22:00:51 +0200

    Update tarifs-pdf.php

--- tarifs/tarifs-pdf.php
+++ tarifs/tarifs-pdf.php
@@ -1,9 +1,15 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define('NOLOGIN', 1);
 define('NOCSRFCHECK', 1);
 define('NOREQUIREUSER', 1);
 define('NOREQUIREMENU', 1);
 define('NOREQUIREHTML', 1);
 
-echo 'FICHIER TARIFS PDF ACCESSIBLE';
+require_once dirname(__DIR__, 2).'/../main.inc.php';
+
+echo 'DOLIBARR CHARGE CORRECTEMENT';
